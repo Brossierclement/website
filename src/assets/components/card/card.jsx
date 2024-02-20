edc3bb91d36c2178ed79c65f498c711f317eea9f
clement brossier <brossier.clement@proton.me>
2024-02-20 21:39:44 +0100

    chore: update data.json and card component

--- src/assets/components/card/card.jsx
+++ src/assets/components/card/card.jsx
@@ -4,10 +4,23 @@ function Card({ data }) {
   return (
     <Link className="card">
       <img className="card-logo" src={data.logo} alt={`logo de ${data.name}`} />
-      <section className="card-infos">
-        <h3>{data.name}</h3>
+      <div className="card-infos">
+        <section className="card-top">
+          <h3>{data.name}</h3>
+          <div className="card-types">
+            {data ? (
+              data.types.map((item, i) => (
+                <p className="type" key={i}>
+                  {item}
+                </p>
+              ))
+            ) : (
+              <p>Chargement...</p>
+            )}
+          </div>
+        </section>
         <p>{data.title}</p>
-      </section>
+      </div>
     </Link>
   );
 }
