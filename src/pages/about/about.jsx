b8f8c395921e3fad0470425e1f27987f3032973f
clement brossier <brossier.contact@gmail.com>
2024-08-10 15:12:00 +0200

    feat: update about branch

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -15,9 +15,23 @@ function About() {
       <p>
         Actuellement je n'ai pas encore eu l'opportunité de faire mes preuves au
         sein d'une entreprise, cependant je compte bien atteindre cet objectif
-        étant persuadé que mes compétences techniques, mais également hunaine
+        étant persuadé que mes compétences techniques, mais également humaine
         peuvent etres utiles.
       </p>
+      <p>
+        Pour l'instant je crée des petits projets qui me permettent d'entretenir
+        mes compétences, de les améliorer, mais également d'en apprendre de
+        nouvelles. En géneral cela tourne autour de sujets que j'aime ou alors
+        pour rendre service à mon entourage.
+      </p>
+      <p>
+        En dehors du code, j'aime créer des vidéos, faire du streaming, lire des
+        livres (en ce moment : Docteur Sleep, de Stephen King), faire du JDR
+        avec mes amis et passer du temps en famille. <br />
+        Consultez l'onglet "Contenu" pour en savoir plus.
+      </p>
+      <p>N'hésitez à me faire signe, je vous repondrez au plus vite!</p>
+      <p>Restez fort!</p>
     </section>
   );
 }
